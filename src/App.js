c1f60bd98cbf050d28218fec2a6a7f96ba87cc82
Daphné bordalis <daphne.bordalis@gmail.com>
2018-03-03 10:28:43 +0100

    list creation ok missing delet&boolean

--- src/App.js
+++ src/App.js
@@ -1,21 +1,77 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
-
+import Task from './Task'
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      taskName: '',
+      taskDone:'false',
+      tasks: []
+    };
+
+    this.removeTask = this.removeTask.bind(this);
+  }
+
+componentDidMount(){
+  axios.get('https://todo-test-mona.herokuapp.com/tasks')
+  .then(tasks => this.setState({
+    tasks: tasks.data
+  }));
+
+}
+
+handleChange(e) {
+  this.setState({
+    taskName: e.target.value
+  });
+}
+
+handleSubmit(e) {
+  // Bloque le refresh du navigateur
+  e.preventDefault();
+
+  // Envoie les informations du formulaire en back 
+  axios
+    .post('https://todo-test-mona.herokuapp.com/tasks', {
+      name: this.state.taskName
+    })
+    .then(result => {
+      console.log(result.data.error);
+      this.setState({
+        taskName: ''
+      });
+      this.updateList();
+    });
+}
+
+removeTask() {
+  this.updateList();
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Ask Mona To Do!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+        <input type="text" value={this.state.taskName} onChange={this.handleChange.bind(this)} />
+          <button type="submit"> + TASK </button>
+          </form>
+      
+      <div className="tasksgo">
+      {this.state.tasks.map(task => <Task name={task.name} remove={this.removeTask} key={task.id} id={task.id} />)}
+      
       </div>
-    );
-  }
+      
+      </div>
+          );
+        }
 }
 
 export default App;
